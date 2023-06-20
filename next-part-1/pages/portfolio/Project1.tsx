c96a10b120bfa4b2843d0f245faed606242df9ba
unknown <canhmy04041998@gmail.com>
2023-06-20 17:54:32 +0700

    8. extracting dynamic path segment data (dynamic routes)

---
+++ next-part-1/pages/portfolio/Project1.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Project1 = () => {
+  return <div>the project 1</div>
+}
+
+export default Project1
---
+++ next-part-1/pages/portfolio/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const index = () => {
+  return <div>portfolio page</div>
+}
+
+export default index
